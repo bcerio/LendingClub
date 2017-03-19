0341e4252d8852bb4e0534174d4c7470bcbbdac2
bcerio <bcerio@gmail.com>
2017-03-19 15:07:37 -0700

    see logs

--- build_model.py
+++ build_model.py
@@ -176,7 +176,7 @@ def main():
     #X = imp.transform(X)
     
     # split sample into random subsets for training and testing
-    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.50,random_state=42)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.35,random_state=42)
 
     logger.log('Shape of training sample: %s' % str(X_train.shape))
     logger.log('Shape of test sample: %s' % str(X_test.shape))
